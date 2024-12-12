801f76fd18a342c7af11783c38df83d103231a21
Manuel Palmieri <manuelpalm2002@gmail.com>
2024-12-12 18:34:35 +0100

    image page finita

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,15 +1,20 @@
 const h1 = document.querySelector("h1")
 let dataString = sessionStorage.getItem("data");
-let data = JSON.parse(dataString)
-const img = document.querySelector("img")
+let data = JSON.parse(dataString);
+const img = document.querySelector("img");
+const h2 = document.querySelector("h2");
+const photographer = document.getElementById("photographer");
+
 console.log(data);
 
 document.addEventListener("load", init())
 
 function init(){
-    getImage();
+    getInfo();
 }
 
-function getImage(){
-    img.setAttribute("src", `${data.src.medium}`)
+function getInfo(){
+    img.setAttribute("src", `${data.src.medium}`);
+    h2.innerHTML = `${data.photographer}`;
+    photographer.setAttribute("href", `${data.photographer_url}`)
 }
