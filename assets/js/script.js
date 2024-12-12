87cbdb229d375243adf9a8ac5a60815e0fdd54f1
Manuel Palmieri <manuelpalm2002@gmail.com>
2024-12-12 16:19:54 +0100

    btnLoad2 fatto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 let dataMountains
 const btnLoad = document.getElementById("btnLoad");
+const btnLoad2 = document.getElementById("btnLoad2")
 const imgDoggo = document.querySelectorAll(".imgDoggo");
+
+document.addEventListener("load", init())
+
+function init() {
+    getApi();
+    getApi2();
+}
 
 async function getApi() {
     await fetch("https://api.pexels.com/v1/search?query=mountains", {
@@ -11,17 +19,34 @@ async function getApi() {
         .then(response => response.json())
         .then(data => {
             dataMountains = data.photos;
-            console.log(dataMountains[1].src.medium);
-
-        });
+        })
+        .catch(error => console.log(error))
 }
-getApi();
 
+async function getApi2() {
+    await fetch("https://api.pexels.com/v1/search?query=sunrise", {
+        headers: {
+            Authorization: 'wTzX1CHG8w6AK8dYAD8cvPXV4bhucR2JXkEEQYxmh81fv7kgOx5mHpNZ'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            dataSunrise = data.photos;
+        })
+        .catch(error => console.log(error))
+}
 
 btnLoad.addEventListener("click", function (e) {
     e.preventDefault();
     for(i=0; i<imgDoggo.length; i++){
         imgDoggo[i].setAttribute("src", `${dataMountains[i].src.medium}`)
     }
+})
 
+
+btnLoad2.addEventListener("click", function (e) {
+    e.preventDefault();
+    for(i=0; i<imgDoggo.length; i++){
+        imgDoggo[i].setAttribute("src", `${dataSunrise[i].src.medium}`)
+    }
 })
